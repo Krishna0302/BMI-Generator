7e840db8f5395ea8268711b4d96f303f2e870728
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-12-20 06:46:28 +0000

    js

--- 03_script.js
+++ 03_script.js
@@ -9,6 +9,12 @@ form.addEventListener('submit', function (e){
     const result = document.querySelector('#result')
 
     if(height==='' || height < 0 || isNaN(height)){
-
-    }
+      result.innerHTML = "Please give a valid height" + height
+    } else if(weight==='' || weight < 0 || isNaN(weight)){
+        result.innerHTML = "Please give a valid weight" + weight
+      } else {
+        const bmi = (weight/((height*height)/1000)).toFixed(2)
+        // show the result
+        result.innerHTML = <span>bmi</span>
+      }
 })
