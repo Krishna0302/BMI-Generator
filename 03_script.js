6c84b5e49dfca6b8afdd6b79a84cdb3c4982f071
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-12-20 07:33:46 +0000

    js

--- 03_script.js
+++ 03_script.js
@@ -1,20 +1,20 @@
-const form = document.querySelector("form")
+const form = document.querySelector('form')
 // this usecase will give empty value
 // const weight = parseInt(document.querySelector('#weight').value)
 
 form.addEventListener('submit', function (e){
-    e.preventDefault;
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const result = document.querySelector('#result')
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#result');
 
     if(height==='' || height < 0 || isNaN(height)){
-      result.innerHTML = "Please give a valid height" + height
+      result.innerHTML = "Please give a valid height";
     } else if(weight==='' || weight < 0 || isNaN(weight)){
-        result.innerHTML = "Please give a valid weight" + weight
+        result.innerHTML = "Please give a valid weight";
       } else {
         const bmi = (weight/((height*height)/1000)).toFixed(2)
         // show the result
-        result.innerHTML = <span>bmi</span>
+        result.innerHTML = "Your BMI is: " + bmi;
       }
-})
+});
